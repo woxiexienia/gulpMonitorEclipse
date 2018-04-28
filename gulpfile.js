4b3956d67d49a77c7512b0ce92daa269f11d7d7a
qh931725663 <931725663@qq.com>
2018-04-28 11:30:15 +0800

    1.1
    sass,es6编译, 输出到项目目录及tomcat

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp         = require('gulp'),
     rename       = require('gulp-rename'),
     babel        = require('gulp-babel'),
     sass         = require('gulp-sass'),
-    cssmin       = require('gulp-clean-css'),					//压缩css
+    cssmin       = require('gulp-clean-css'),				//压缩css
     autoprefixer = require('gulp-autoprefixer'),		//添加浏览器前缀
     clean        = require('gulp-clean'),						//清理文目标文件夹
     csso         = require('gulp-csso'),						//合并css属性
@@ -14,102 +14,79 @@ var gulp         = require('gulp'),
 
 var target = 'D:/apache-tomcat-8.0.43/webapps/mkh1.0/system/';
 
-var srcFile               = 'D:/workspace/mkh1.0/WebRoot/system/',
-    srcFileView           = srcFile+'view/**/*.html',
-    srcFileViews2         = srcFile+'views2/**/*.html',
-    srcFileJs             = srcFile+'view/**/js/*.js',
-    srcFileJs2            = srcFile+'view/**/js2/*.js',
-    srcFileCss            = srcFile+'view/**/css/*.css',
-    srcFileSassCommon     = srcFile+'sass2/common/**/*.scss',
-    srcFileSassController = srcFile+'sass2/controller/**/*.scss',
-    srcFileSassWap        = srcFile+'sass2/wap/**/*.scss',
-    srcFileImg            = srcFile+'css/img/**/*.{jpg,png}'
+var srcFile     = 'D:/workspace/mkh1.0/WebRoot/system/',
+    srcFileView = srcFile+'view/**/*.html',
+    srcFileJs   = srcFile+'view/**/js/*.js',
+    srcFileJs2  = srcFile+'view/**/js2/*.js',
+    srcFileCss  = srcFile+'view/**/css/*.css',
+    srcFileSass = srcFile+'view/**/sass/*.scss',
+    srcFileImg  = srcFile+'view/**/css/images/*.{jpg,png}'
 	;
-// es6 -> es3
-gulp.task('compileEs6', function() {
-    gulp.src(srcFileJs2)
-      .pipe(changed(target+'view'))
-      .pipe(babel())
-      .pipe(rename(function (path) {
-        path.dirname = path.dirname.replace('js2', 'js')
-      }))
-      .pipe(gulp.dest(srcFile+'view'));
 
-});
 /*由于外部编辑器修改后，需要七八秒eclipse才能监听到文件，因此直接复制文件到eclipse发布的目录*/
+
+// html
 gulp.task('copyViews', function() {
     gulp.src(srcFileView)
       .pipe(changed(target+'view'))
       .pipe(gulp.dest(target+'view'));
 
 });
-gulp.task('copyViews2', function() {
-    gulp.src(srcFileViews2)
-      .pipe(changed(srcFileViews2))
-      .pipe(gulp.dest(target+'view'));
-
+// es6
+/* presets-es2015 需要在全局安装不然无法编译 */
+gulp.task('compileEs6', function() {
+    gulp.src(srcFileJs2)
+      .pipe(changed(target+'view'))
+      .pipe(babel({
+        presets: ['es2015']g
+      }))
+      .pipe(rename(function (path) {
+        path.dirname = path.dirname.replace('js2', 'js')
+      }))
+      .pipe(gulp.dest(srcFile+'view'));
 });
+// js
 gulp.task('copyJs', function() {
     gulp.src(srcFileJs)
       .pipe(changed(srcFileJs))
       .pipe(gulp.dest(target+'view'));
 
 });
+// css
 gulp.task('copyCss', function() {
     gulp.src(srcFileCss)
       .pipe(changed(srcFileCss))
       .pipe(gulp.dest(target+'css'));
 
 });
+// img
 gulp.task('copyImg', function() {
     gulp.src(srcFileImg)
       .pipe(changed(srcFileImg))
       .pipe(gulp.dest(target+'css/img'));
 
 });
-
-gulp.task('sassCommon',function () {			
-    gulp.src(srcFileSassCommon)
-    	.pipe(sass())
-	    .pipe(cssmin())
-	    .pipe(autoprefixer())
-	    .pipe(csso())
-	    .pipe(csslint())
-	    .pipe(gulp.dest(srcFile+'css/common2'))
-	    .pipe(gulp.dest(target+'css/common2'));	//同时更改到eclipse发布的目录下
-});
-
-gulp.task('sassController',function () {			
-    gulp.src(srcFileSassController)
-    	.pipe(sass())
-	    .pipe(cssmin())
-	    .pipe(autoprefixer())
-	    .pipe(csso())
-	    .pipe(csslint())
-	    .pipe(gulp.dest(srcFile+'css/common2'))
-	    .pipe(gulp.dest(target+'css/common2'));
-});
-
-gulp.task('sassWap',function () {			
-    gulp.src(srcFileSassWap)
-    	.pipe(sass())
-	    .pipe(cssmin())
-	    .pipe(autoprefixer())
-	    .pipe(csso())
-	    .pipe(csslint())
-	    .pipe(gulp.dest(srcFile+'css/mobile'))
-	    .pipe(gulp.dest(target+'css/mobile'));
+// sass
+gulp.task('sassCommon',function () {      
+    gulp.src(srcFileSass)
+      .pipe(sass()).on('error', sass.logError)
+      .pipe(cssmin())
+      .pipe(autoprefixer())
+      .pipe(csso())
+      // .pipe(csslint())
+      .pipe(rename(function (path) {
+        path.dirname = path.dirname.replace('sass', 'css')
+        console.log(path);
+      }))
+      .pipe(gulp.dest(srcFile+'view'))
 });
 
 gulp.task("autowatch",function(){
 	gulp.watch([srcFileView],['copyViews']);		
-	gulp.watch([srcFileViews2],['copyViews2']);	
 	gulp.watch([srcFileJs],['copyJs']);
-    gulp.watch([srcFileJs2],['compileEs6']); 
-	//gulp.watch([srcFileCss],['copyCss']);	
-	gulp.watch([srcFileSassCommon],['sassCommon']);	
-	gulp.watch([srcFileSassController],['sassController']);	
-	gulp.watch([srcFileSassWap],['sassWap']);	
+  gulp.watch(srcFileJs2,['compileEs6']); 
+	gulp.watch([srcFileCss],['copyCss']);	
+	gulp.watch([srcFileSass],['sassCommon']);	
 	gulp.watch([srcFileImg],['copyImg']);	
 });
 
